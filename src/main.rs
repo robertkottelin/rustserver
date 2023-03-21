62a6a313d182011c835932bf118f68c84617cc71
robertkottelin <robert.kottelin@gmail.com>
2023-03-21 22:40:29 +0100

    write errors

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use sqlx::sqlite::SqlitePool;
-use std::env;
-use futures_util::stream::TryStreamExt;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, post, get};
+use rusqlite::{Connection, params};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+use actix_files::Files;
 
-
-async fn submit_text(pool: web::Data<SqlitePool>, text: web::Json<String>) -> impl Responder {
-    let result = sqlx::query(
-        r#"
-        INSERT INTO texts (content) VALUES (?);
-        "#,
-    )
-    .bind(&text.0)
-    .execute(pool.as_ref())
-    .await;
-
-    match result {
-        Ok(_) => HttpResponse::Ok().body("Text added successfully."),
-        Err(e) => {
-            eprintln!("Error while adding text: {}", e);
-            HttpResponse::InternalServerError().body("Failed to add text.")
-        }
-    }
+#[derive(Debug, Deserialize, Serialize)]
+struct Text {
+    id: i32,
+    content: String,
 }
 
-async fn get_texts(pool: web::Data<SqlitePool>) -> impl Responder {
-    let mut texts = Vec::new();
-    let mut rows = sqlx::query("SELECT content FROM texts")
-        .fetch(pool.as_ref());
+// ERROR HERE, NOT RIGHT TYPE OF DATA FROM FRONTEND TO DB
+#[post("/submit_data")]
+async fn submit_data(conn: web::Data<Arc<Mutex<Connection>>>, text: web::Json<Text>) -> impl Responder {
+    let conn = conn.lock().unwrap();
+    let mut stmt = conn
+        .prepare("INSERT INTO texts (content) VALUES (?2)")
+        .unwrap();
+    stmt.execute(params![text.content]).unwrap();
+    HttpResponse::Created().finish()
+}
 
-    while let Some(row) = rows.try_next().await.unwrap() {
-        texts.push(row.get("content"));
+#[get("/get_data")]
+async fn get_data(conn: web::Data<Arc<Mutex<Connection>>>) -> impl Responder {
+    let conn = conn.lock().unwrap();
+    let mut stmt = conn.prepare("SELECT id, content FROM texts").unwrap();
+    let rows = stmt.query_map([], |row| {
+        Ok(Text {
+            id: row.get(0)?,
+            content: row.get(1)?,
+        })
+    }).unwrap();
+    
+    let mut texts = Vec::new();
+    for text_result in rows {
+        texts.push(text_result.unwrap());
     }
 
     HttpResponse::Ok().json(texts)
@@ -38,18 +43,18 @@ async fn get_texts(pool: web::Data<SqlitePool>) -> impl Responder {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Set up the database
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = SqlitePool::connect(&database_url)
-        .await
-        .expect("Failed to create connection pool");
+    let conn = Arc::new(Mutex::new(Connection::open("src/data.db").unwrap()));
 
-    // Start the server
+
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .route("/submit_text", web::post().to(submit_text))
-            .route("/get_texts", web::get().to(get_texts))
+            .app_data(web::Data::new(conn.clone()))
+            .service(submit_data)
+            .service(get_data)
+            .service(Files::new("/", "./static").index_file("index.html")) // Serve static files
+
     })
+
     .bind("127.0.0.1:8080")?
     .run()
     .await
---
+++ static/index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    loadData();
+  
+    const form = document.getElementById("text-form");
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+  
+      const input = document.getElementById("text-input");
+      const content = input.value;
+      const data = { content };
+  
+      try {
+        const response = await fetch("/submit_data", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+  
+        if (response.status === 201) {
+          console.log("Data submitted successfully");
+          input.value = ""; // Clear the input field
+          loadData(); // Refresh the data on the page
+        } else {
+          console.error("Failed to submit data");
+        }
+      } catch (error) {
+        console.error("Error submitting data:", error);
+      }
+    });
+  });
+  
+  async function loadData() {
+    const response = await fetch("/get_data");
+    const data = await response.json();
+  
+    const container = document.getElementById("texts-container");
+    container.innerHTML = "";
+  
+    data.forEach((text) => {
+      const div = document.createElement("div");
+      div.textContent = `ID: ${text.id}, Content: ${text.content}`;
+      container.appendChild(div);
+    });
+  }
+  
