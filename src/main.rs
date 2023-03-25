d0edf70f611324d1b39f730771b14854e80ace2e
robertkottelin <robert.kottelin@gmail.com>
2023-03-25 12:05:20 +0100

    minimal rust server working (read and write)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, post, get};
-use rusqlite::{Connection, params};
+use actix_files::Files;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use rusqlite::{params, Connection};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
-use actix_files::Files;
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Text {
+struct Data {
     id: i32,
     content: String,
 }
 
-// ERROR HERE, NOT RIGHT TYPE OF DATA FROM FRONTEND TO DB -- FIX
+#[derive(Debug, Deserialize, Serialize)]
+struct IncomingData {
+    content: String,
+}
+
 #[post("/submit_data")]
-async fn submit_data(conn: web::Data<Arc<Mutex<Connection>>>, text: web::Json<Text>) -> impl Responder {
+async fn submit_data(
+    conn: web::Data<Arc<Mutex<Connection>>>,
+    new_text: web::Json<IncomingData>,
+) -> impl Responder {
     let conn = conn.lock().unwrap();
     let mut stmt = conn
-        .prepare("INSERT INTO texts (content) VALUES (?2)")
+        .prepare("INSERT INTO texts (content) VALUES (?1)")
         .unwrap();
-    stmt.execute(params![text.content]).unwrap();
+    stmt.execute(params![new_text.content]).unwrap();
     HttpResponse::Created().finish()
 }
 
@@ -25,13 +32,15 @@ async fn submit_data(conn: web::Data<Arc<Mutex<Connection>>>, text: web::Json<Te
 async fn get_data(conn: web::Data<Arc<Mutex<Connection>>>) -> impl Responder {
     let conn = conn.lock().unwrap();
     let mut stmt = conn.prepare("SELECT id, content FROM texts").unwrap();
-    let rows = stmt.query_map([], |row| {
-        Ok(Text {
-            id: row.get(0)?,
-            content: row.get(1)?,
+    let rows = stmt
+        .query_map([], |row| {
+            Ok(Data {
+                id: row.get(0)?,
+                content: row.get(1)?,
+            })
         })
-    }).unwrap();
-    
+        .unwrap();
+
     let mut texts = Vec::new();
     for text_result in rows {
         texts.push(text_result.unwrap());
@@ -45,16 +54,13 @@ async fn main() -> std::io::Result<()> {
     // Set up the database
     let conn = Arc::new(Mutex::new(Connection::open("src/data.db").unwrap()));
 
-
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(conn.clone()))
             .service(submit_data)
             .service(get_data)
             .service(Files::new("/", "./static").index_file("index.html")) // Serve static files
-
     })
-
     .bind("127.0.0.1:8080")?
     .run()
     .await
